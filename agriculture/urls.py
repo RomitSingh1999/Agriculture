17559456cfe9a1c872f5fd54952c788bd5498fbc
RomitSingh1999 <62353481+RomitSingh1999@users.noreply.github.com>
2020-10-11 16:11:39 +0530

    Update urls.py

--- agriculture/urls.py
+++ agriculture/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     path('',include('crops.urls')),
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
