563b3ec0a307d3ab9d50e57b2c99ae8bcd4a6d1c
RomitSingh1999 <62353481+RomitSingh1999@users.noreply.github.com>
2020-10-15 00:19:19 +0530

    Update urls.py

--- agriculture/urls.py
+++ agriculture/urls.py
@@ -23,6 +23,6 @@ from django.views.static import serve
 urlpatterns = [
     path('',include('crops.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
