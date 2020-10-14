d60e0139115d075c4256275ca33bd41091bbe123
RomitSingh1999 <62353481+RomitSingh1999@users.noreply.github.com>
2020-10-15 00:54:50 +0530

    Update settings.py

--- agriculture/settings.py
+++ agriculture/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/agriculture-10-akatsuki/static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
