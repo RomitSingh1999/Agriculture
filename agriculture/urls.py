d534e150d4ba7b467dbf853b05d09e1815cc1f9e
RomitSingh1999 <62353481+RomitSingh1999@users.noreply.github.com>
2020-10-15 00:48:18 +0530

    Update urls.py

--- agriculture/urls.py
+++ agriculture/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
     #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
